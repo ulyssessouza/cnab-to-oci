ae7006af4c75eeed0ce1aed427c6644905eee5ee
Ulysses Souza <ulysses.souza@docker.com>
2018-12-21 18:02:06 +0100

    Implements the version command with a TODO in as output. Just to be conform with the documentation
    Signed-off-by: Ulysses Souza <ulysses.souza@docker.com>

--- cmd/cnab-to-oci/main.go
+++ cmd/cnab-to-oci/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -11,8 +12,19 @@ func main() {
 		Use:          "cnab-to-oci <subcommand> [options]",
 		SilenceUsage: true,
 	}
-	cmd.AddCommand(fixupCmd(), pushCmd(), pullCmd())
+	cmd.AddCommand(fixupCmd(), pushCmd(), pullCmd(), versionCmd())
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func versionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "version",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("TODO(ulyssessouza) Implement versioning")
+			return nil
+		},
+	}
+	return cmd
 }
